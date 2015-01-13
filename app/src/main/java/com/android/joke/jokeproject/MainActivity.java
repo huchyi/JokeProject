a5222a56e8d5de9284ab702bab2ef02ee104c2ce
huchyi <503683716@qq.com>
2015-01-13 14:30:11 +0800

    android studio commit complete mainFragment

--- app/src/main/java/com/android/joke/jokeproject/MainActivity.java
+++ app/src/main/java/com/android/joke/jokeproject/MainActivity.java
@@ -1,67 +1,94 @@
 package com.android.joke.jokeproject;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 
 
-public class MainActivity extends ActionBarActivity {
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends Activity implements OnClickListener
+{
+    private RelativeLayout mTabMainButton;
+    private RelativeLayout mTabCollectionButton;
+
+    private MainFragment mainFragment;
+    private CollectionFragment collectionFragment;
+
+    private FragmentManager fm;
+    private  FragmentTransaction transaction;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-    }
 
+        // 初始化控件和声明事件
+        mTabMainButton = (RelativeLayout) findViewById(R.id.bleow_two_btn_rl_main);
+        mTabCollectionButton = (RelativeLayout) findViewById(R.id.bleow_two_btn_rl_collection);
+        mTabMainButton.setOnClickListener(this);
+        mTabCollectionButton.setOnClickListener(this);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-
-        return super.onOptionsItemSelected(item);
+        // 设置默认的Fragment
+        setDefaultFragment();
     }
 
     /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
+     * 主界面
+     *
+     * */
+    private void setDefaultFragment(){
+        fm = getFragmentManager();
+        transaction = fm.beginTransaction();
+        if (mainFragment == null){
+            mainFragment = new MainFragment();
         }
+        transaction.replace(R.id.id_fragment_main, mainFragment);
+        // transaction.addToBackStack();
+        // 事务提交
+        transaction.commitAllowingStateLoss();
+        mTabMainButton.setBackgroundColor(getResources().getColor(R.color.bottom_bg));
+        mTabCollectionButton.setBackgroundColor(getResources().getColor(R.color.bottom_bg_while));
+    }
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
+
+    /**
+     * 次界面
+     * **/
+    private void setOtherFragment(){
+        fm = getFragmentManager();
+        transaction = fm.beginTransaction();
+        if (collectionFragment == null){
+            collectionFragment = new CollectionFragment();
+        }
+        transaction.replace(R.id.id_fragment_collection, collectionFragment);
+        // transaction.addToBackStack();
+        // 事务提交
+        transaction.commitAllowingStateLoss();
+        mTabMainButton.setBackgroundColor(getResources().getColor(R.color.bottom_bg_while));
+        mTabCollectionButton.setBackgroundColor(getResources().getColor(R.color.bottom_bg));
+    }
+
+    @Override
+    public void onClick(View v){
+
+        switch (v.getId()){
+            case R.id.bleow_two_btn_rl_main:
+                // 使用当前Fragment的布局替代id_content的控件
+                setDefaultFragment();
+
+                break;
+            case R.id.bleow_two_btn_rl_collection:
+                setOtherFragment();
+
+                break;
         }
     }
+
 }
